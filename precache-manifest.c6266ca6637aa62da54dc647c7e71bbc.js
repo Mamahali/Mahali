e4ef6a23835510df16aad09a6d69f26894e6991e
Mamahali <mamahalimatela74@gmail.com>
2024-11-15 02:28:41 +0200

    Updates

---
+++ precache-manifest.c6266ca6637aa62da54dc647c7e71bbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91f36d676b48c3abb69c8df78f313e6",
+    "url": "/Mahali/index.html"
+  },
+  {
+    "revision": "d416bbd8e2c9795ef5fa",
+    "url": "/Mahali/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cbc20ca1956509734a3e",
+    "url": "/Mahali/static/js/2.1c6a6bbb.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/Mahali/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "d416bbd8e2c9795ef5fa",
+    "url": "/Mahali/static/js/main.465cff34.chunk.js"
+  },
+  {
+    "revision": "d9810b7950452616378b",
+    "url": "/Mahali/static/js/runtime~main.d562d459.js"
+  }
+]);
